3e027b11d323c5bb05f541d7d24eed5f9455d3e9
Cameron Bardell <cambardell3@gmail.com>
2019-12-10 20:03:11 -0500

    commit

---
+++ Footnote2/AddQuoteView.swift
@@ -0,0 +1,21 @@
+//
+//  AddQuoteView.swift
+//  Footnote2
+//
+//  Created by Cameron Bardell on 2019-12-10.
+//  Copyright © 2019 Cameron Bardell. All rights reserved.
+//
+
+import SwiftUI
+
+struct AddQuoteView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct AddQuoteView_Previews: PreviewProvider {
+    static var previews: some View {
+        AddQuoteView()
+    }
+}
