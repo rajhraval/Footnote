8cf3fcae1cddb87fc29e5a861e58d82f721a9687
Cameron Bardell <cambardell3@gmail.com>
2019-12-10 19:34:09 -0500

    Initial Commit

---
+++ Footnote2/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Footnote2
+//
+//  Created by Cameron Bardell on 2019-12-10.
+//  Copyright © 2019 Cameron Bardell. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
