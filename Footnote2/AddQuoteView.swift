9835e2f866a6b84a17d2904232b85a7b1a100a80
Cameron Bardell <cambardell3@gmail.com>
2020-01-17 20:23:14 -0500

    Improved author and title suggestions, but required two suggestion views, one for each fetch request.

--- Footnote2/AddQuoteView.swift
+++ Footnote2/AddQuoteView.swift
@@ -44,12 +44,11 @@ struct AddQuoteView: View {
                     .padding([.leading, .trailing])
                     .disableAutocorrection(true)
                 
-                SuggestionsView(filter: self.author, filterType: "author", author: self.$author, title: self.$title).environment(\.managedObjectContext, self.managedObjectContext)
+                AuthorSuggestionsView(filter: self.author, text: self.$author).environment(\.managedObjectContext, self.managedObjectContext)
                     .frame(width: geometry.size.width - 30, height: 50)
-                SuggestionsView(filter: self.title, filterType: "title", author: self.$author, title: self.$title).environment(\.managedObjectContext, self.managedObjectContext)
+                
+                TitleSuggestionsView(filter: self.title, text: self.$title).environment(\.managedObjectContext, self.managedObjectContext)
                 .frame(width: geometry.size.width - 30, height: 50)
-                
-                
                 
                 Text("Add Quote").font(.title).foregroundColor(Color.white)
                 Text("Swipe to dismiss").font(.footnote).foregroundColor(Color.white)
@@ -69,6 +68,14 @@ struct AddQuoteView: View {
         quote.author = self.author
         quote.dateCreated = Date()
         
+        let authorItem = Author(context: self.managedObjectContext)
+        authorItem.text = self.author
+        authorItem.count += 1
+        
+        let titleItem = Title(context: self.managedObjectContext)
+        titleItem.text = self.title
+        titleItem.count += 1
+        
         do {
             try self.managedObjectContext.save()
         } catch {
@@ -81,65 +88,53 @@ struct AddQuoteView: View {
     }
 }
 
-struct SuggestionsView: View {
+struct AuthorSuggestionsView: View {
     @Environment(\.managedObjectContext) var managedObjectContext
-    var fetchRequest: FetchRequest<Quote>
-    var filterType: String
+    var fetchRequest: FetchRequest<Author>
     
-    @Binding var author: String
-    @Binding var title: String
+    @Binding var text: String
     
-    init(filter: String, filterType: String, author: Binding<String>, title: Binding<String>) {
-        fetchRequest = FetchRequest<Quote>(entity: Quote.entity(), sortDescriptors: [
-            NSSortDescriptor(keyPath: \Quote.dateCreated, ascending: false)
+    init(filter: String, text: Binding<String>) {
+        fetchRequest = FetchRequest<Author>(entity: Author.entity(), sortDescriptors: [
+            NSSortDescriptor(keyPath: \Author.count, ascending: false)
             ], predicate: NSCompoundPredicate(
                 type: .or,
                 subpredicates: [
                     // [cd] = case and diacritic insensitive
-                    NSPredicate(format: "\(filterType) CONTAINS[cd] %@", filter),
-                    
+                    NSPredicate(format: "text CONTAINS[cd] %@", filter),
                 ]
         ))
         
         // Initialize a binding variable
-        self._author = author
-        self._title = title
-        self.filterType = filterType
-        
+        self._text = text
         
     }
     var body: some View {
         GeometryReader { geometry in
             
             HStack {
-                
                 // Couldn't figure out fetchlimit with SwiftUI, leading to this monstrosity.
                 if self.fetchRequest.wrappedValue.count >= 2 {
                     Group {
                         Button(action: {
-                            if self.filterType == "author" {
-                                self.author = self.fetchRequest.wrappedValue[0].author ?? ""
-                            } else if self.filterType == "title" {
-                                self.title = self.fetchRequest.wrappedValue[0].title ?? ""
-                            }
+                            
+                            self.text = self.fetchRequest.wrappedValue[0].text ?? ""
+                            
                         }) {
-                            Text(self.filterType == "author" ? self.fetchRequest.wrappedValue[0].author ?? "" : self.fetchRequest.wrappedValue[0].title ?? "")
+                            Text(self.fetchRequest.wrappedValue[0].text ?? "")
                                 .foregroundColor(Color.footnoteOrange)
                             .padding(5)
                             .background(Color.white)
                             .cornerRadius(10)
                             .frame(width: geometry.size.width / 2 - 5, height: 50)
-                                
                         }
                         
                        Button(action: {
-                            if self.filterType == "author" {
-                                self.author = self.fetchRequest.wrappedValue[1].author ?? ""
-                            } else if self.filterType == "title" {
-                                self.title = self.fetchRequest.wrappedValue[1].title ?? ""
-                            }
+                            
+                                self.text = self.fetchRequest.wrappedValue[1].text ?? ""
+                            
                         }) {
-                            Text(self.filterType == "author" ? self.fetchRequest.wrappedValue[1].author ?? "" : self.fetchRequest.wrappedValue[1].title ?? "")
+                            Text(self.fetchRequest.wrappedValue[1].text ?? "")
                                 .foregroundColor(Color.footnoteOrange)
                             .padding(5)
                             .background(Color.white)
@@ -148,15 +143,13 @@ struct SuggestionsView: View {
                         }
                     }
                 } else {
-                    ForEach(self.fetchRequest.wrappedValue, id: \.self) { quote in
+                    ForEach(self.fetchRequest.wrappedValue, id: \.self) { item in
                         Button(action: {
-                            if self.filterType == "author" {
-                                self.author = quote.author ?? ""
-                            } else if self.filterType == "title" {
-                                self.title = quote.title ?? ""
-                            }
+                            
+                                self.text = item.text ?? ""
+            
                         }) {
-                            Text(self.filterType == "author" ? quote.author ?? "" : quote.title ?? "")
+                            Text(item.text ?? "")
                                 .foregroundColor(Color.footnoteOrange)
                             .padding(5)
                             .background(Color.white)
@@ -165,17 +158,88 @@ struct SuggestionsView: View {
                         }
                     }
                 }
-                
                 Spacer()
-                
             }
         }
-        
-        
+    }
+}
+
+struct TitleSuggestionsView: View {
+    @Environment(\.managedObjectContext) var managedObjectContext
+    var fetchRequest: FetchRequest<Title>
+    
+    @Binding var text: String
+    
+    init(filter: String, text: Binding<String>) {
+        fetchRequest = FetchRequest<Title>(entity: Title.entity(), sortDescriptors: [
+            NSSortDescriptor(keyPath: \Title.count, ascending: false)
+            ], predicate: NSCompoundPredicate(
+                type: .or,
+                subpredicates: [
+                    // [cd] = case and diacritic insensitive
+                    NSPredicate(format: "text CONTAINS[cd] %@", filter),
+                ]
+        ))
+        print("init")
+        // Initialize a binding variable
+        self._text = text
         
     }
-    
+    var body: some View {
+        GeometryReader { geometry in
+            
+            HStack {
+                // Couldn't figure out fetchlimit with SwiftUI, leading to this monstrosity.
+                if self.fetchRequest.wrappedValue.count >= 2 {
+                    Group {
+                        Button(action: {
+                            
+                            self.text = self.fetchRequest.wrappedValue[0].text ?? ""
+                            
+                        }) {
+                            Text(self.fetchRequest.wrappedValue[0].text ?? "")
+                                .foregroundColor(Color.footnoteOrange)
+                            .padding(5)
+                            .background(Color.white)
+                            .cornerRadius(10)
+                            .frame(width: geometry.size.width / 2 - 5, height: 50)
+                        }
+                        
+                       Button(action: {
+                            
+                                self.text = self.fetchRequest.wrappedValue[1].text ?? ""
+
+                        }) {
+                            Text(self.fetchRequest.wrappedValue[1].text ?? "")
+                                .foregroundColor(Color.footnoteOrange)
+                            .padding(5)
+                            .background(Color.white)
+                            .cornerRadius(10)
+                            .frame(width: geometry.size.width / 2 - 5, height: 50)
+                        }
+                    }
+                } else {
+                    ForEach(self.fetchRequest.wrappedValue, id: \.self) { item in
+                        Button(action: {
+                            
+                                self.text = item.text ?? ""
+            
+                        }) {
+                            Text(item.text ?? "")
+                                .foregroundColor(Color.footnoteOrange)
+                            .padding(5)
+                            .background(Color.white)
+                            .cornerRadius(10)
+                            .frame(width: geometry.size.width / 2 - 5, height: 50)
+                        }
+                    }
+                }
+                Spacer()
+            }
+        }
+    }
 }
+
 
 struct AddQuoteView_Previews: PreviewProvider {
     static var previews: some View {
