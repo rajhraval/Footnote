7c135d238b4d9db9075c6a876ac9c9b5fb8f84a8
Cameron Bardell <cambardell3@gmail.com>
2020-10-03 15:07:43 -0400

    Fixing my own source control mistakes.

--- Footnote/Main Views/ContentView.swift
+++ Footnote/Main Views/ContentView.swift
@@ -9,165 +9,165 @@
 import SwiftUI
 
 struct ContentView: View {
+  
+  @Environment(\.managedObjectContext) var managedObjectContext
+  
+  //Controls translation of AddQuoteView
+  @State private var offset: CGSize = .zero
+  @State var search = ""
+  @State var showModal = false
+  @State var showView: ContentViewModals = .addQuoteView
+  
+  @State private var refreshing = false
+  private var didSave =  NotificationCenter.default.publisher(for: .NSManagedObjectContextDidSave)
+  
+  
+  @FetchRequest(
+    entity: Quote.entity(),
+    sortDescriptors: [
+      NSSortDescriptor(keyPath: \Quote.dateCreated, ascending: false)
+    ]
+  ) var quotes: FetchedResults<Quote>
+  
+  var body: some View {
     
-    @Environment(\.managedObjectContext) var managedObjectContext
-    
-    //Controls translation of AddQuoteView
-    @State private var offset: CGSize = .zero
-    @State var search = ""
-    @State var showModal = false
-    @State var showView: ContentViewModals = .addQuoteView
-    
-    @State private var refreshing = false
-    private var didSave =  NotificationCenter.default.publisher(for: .NSManagedObjectContextDidSave)
-
-    
-    @FetchRequest(
-        entity: Quote.entity(),
-        sortDescriptors: [
-            NSSortDescriptor(keyPath: \Quote.dateCreated, ascending: false)
-        ]
-    ) var quotes: FetchedResults<Quote>
-    
-    var body: some View {
+    NavigationView {
+      VStack {
+        TextField("Search", text: self.$search)
+          .textFieldStyle(RoundedBorderTextFieldStyle())
+          .padding([.leading, .trailing, .top])
         
-            NavigationView {
-                    VStack {
-                        TextField("Search", text: self.$search)
-                            .textFieldStyle(RoundedBorderTextFieldStyle())
-                            .padding([.leading, .trailing, .top])
-                        
-                        
-                        if self.search != "" {
-                            FilteredList(filter: self.search).environment(\.managedObjectContext, self.managedObjectContext)
-                        } else {
-                            
-                            List {
-                                ForEach(self.quotes, id: \.self) { quote in
-                                    // Issue #17: Pass Media type to the detail view
-                                    NavigationLink(destination: QuoteDetailView(text: quote.text ?? "",
-                                                title: quote.title ?? "",
-                                                author: quote.author ?? "",
-                                                mediaType: MediaType(rawValue: Int(quote.mediaType)) ?? MediaType.book,
-                                                quote: quote
-                                    ).environment(\.managedObjectContext, self.managedObjectContext)) {
-                                        QuoteItemView(quote: quote)
-                                    }
-                                    .onReceive(self.didSave) { _ in
-                                        self.refreshing.toggle()
-                                        print("refresh")
-                                    }
-                                    
-                                    
-                                }.onDelete(perform: self.removeQuote)
+        
+        if self.search != "" {
+          FilteredList(filter: self.search).environment(\.managedObjectContext, self.managedObjectContext)
+        } else {
+          
+          List {
+            ForEach(self.quotes, id: \.self) { quote in
+              // Issue #17: Pass Media type to the detail view
+              NavigationLink(destination: QuoteDetailView(text: quote.text ?? "",
+                                                          title: quote.title ?? "",
+                                                          author: quote.author ?? "",
+                                                          mediaType: MediaType(rawValue: Int(quote.mediaType)) ?? MediaType.book,
+                                                          quote: quote
+              ).environment(\.managedObjectContext, self.managedObjectContext)) {
+                QuoteItemView(quote: quote)
+              }
+              .onReceive(self.didSave) { _ in
+                self.refreshing.toggle()
+                print("refresh")
+              }
+              
+              
+            }.onDelete(perform: self.removeQuote)
+            
+          }
+          .listStyle(PlainListStyle())
+          .navigationBarTitle("Footnote", displayMode: .inline)
+          .navigationBarItems(trailing:
                                 
-                            }
-                            .listStyle(PlainListStyle())
-                            .navigationBarTitle("Footnote", displayMode: .inline)
-                            .navigationBarItems(trailing:
-                                    
-                                    Button(action: {
-                                        self.showView = .addQuoteView
-                                        self.showModal.toggle()
-                                    } ) {
-                                    Image(systemName: "plus")
+                                Button(action: {
+                                  self.showView = .addQuoteView
+                                  self.showModal.toggle()
+                                } ) {
+                                  Image(systemName: "plus")
                                 } )
-                        }
-                    }
-        }.accentColor(Color.footnoteRed)
-        .sheet(isPresented: $showModal) {
-            if self.showView == .addQuoteView {
-
-                AddQuoteUIKit(showModal: $showModal).environment(\.managedObjectContext, self.managedObjectContext)
-
-            } else {
-                // modals...
-            }
-
         }
-    }
+      }
+    }.sheet(isPresented: $showModal) {
+      if self.showView == .addQuoteView {
+        
+        AddQuoteUIKit(showModal: $showModal).environment(\.managedObjectContext, self.managedObjectContext)
+        
+      } else {
+        // modals...
+      }
+      
+    }.accentColor(Color.footnoteRed)
     
-    func removeQuote(at offsets: IndexSet) {
-        for index in offsets {
-            let quote = quotes[index]
-            managedObjectContext.delete(quote)
-        }
-        do {
-            try managedObjectContext.save()
-        } catch {
-            // handle the Core Data error
-        }
+  }
+  
+  func removeQuote(at offsets: IndexSet) {
+    for index in offsets {
+      let quote = quotes[index]
+      managedObjectContext.delete(quote)
     }
+    do {
+      try managedObjectContext.save()
+    } catch {
+      // handle the Core Data error
+    }
+  }
 }
 
 /// contentView modals
 enum ContentViewModals {
-    case addQuoteView
-    case contributorView
+  case addQuoteView
+  case contributorView
 }
 
 // To preview with CoreData
 #if DEBUG
 struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
-        return Group {
-            ContentView().environment(\.managedObjectContext, context).environment(\.colorScheme, .light)
-            
-        }
-        
+  static var previews: some View {
+    let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
+    return Group {
+      ContentView().environment(\.managedObjectContext, context).environment(\.colorScheme, .light)
+      
     }
+    
+  }
 }
 #endif
 
 struct FilteredList: View {
-    @Environment(\.managedObjectContext) var managedObjectContext
-    @State var showImageCreator = false
-    var fetchRequest: FetchRequest<Quote>
+  @Environment(\.managedObjectContext) var managedObjectContext
+  @State var showImageCreator = false
+  var fetchRequest: FetchRequest<Quote>
+  
+  
+  init(filter: String) {
+    fetchRequest = FetchRequest<Quote>(entity: Quote.entity(), sortDescriptors: [
+      NSSortDescriptor(keyPath: \Quote.dateCreated, ascending: false)
+    ], predicate: NSCompoundPredicate(
+      type: .or,
+      subpredicates: [
+        // [cd] = case and diacritic insensitive
+        NSPredicate(format: "text CONTAINS[cd] %@", filter),
+        NSPredicate(format: "author CONTAINS[cd] %@", filter),
+        NSPredicate(format: "title CONTAINS[cd] %@", filter)
+      ]
+    ))
+  }
+  
+  var body: some View {
     
-    
-    init(filter: String) {
-        fetchRequest = FetchRequest<Quote>(entity: Quote.entity(), sortDescriptors: [
-            NSSortDescriptor(keyPath: \Quote.dateCreated, ascending: false)
-            ], predicate: NSCompoundPredicate(
-                type: .or,
-                subpredicates: [
-                    // [cd] = case and diacritic insensitive
-                    NSPredicate(format: "text CONTAINS[cd] %@", filter),
-                    NSPredicate(format: "author CONTAINS[cd] %@", filter),
-                    NSPredicate(format: "title CONTAINS[cd] %@", filter)
-                ]
-        ))
+    NavigationView {
+      List {
+        ForEach(fetchRequest.wrappedValue, id: \.self) { quote in
+          // Issue #17: Pass Media type to the detail view
+          NavigationLink(destination: QuoteDetailView(text: quote.text ?? "",
+                                                      title: quote.title ?? "",
+                                                      author: quote.author ?? "",
+                                                      mediaType: MediaType(rawValue: Int(quote.mediaType)) ?? MediaType.book,
+                                                      quote: quote)) {
+            QuoteItemView(quote: quote)
+          }
+        }.onDelete(perform: self.removeQuote)
+      }
+    }.navigationBarTitle("")
+    .navigationBarHidden(true)
+  }
+  
+  func removeQuote(at offsets: IndexSet) {
+    for index in offsets {
+      let quote = fetchRequest.wrappedValue[index]
+      managedObjectContext.delete(quote)
     }
-    
-    var body: some View {
-        
-        NavigationView {
-            List {
-                ForEach(fetchRequest.wrappedValue, id: \.self) { quote in
-                    // Issue #17: Pass Media type to the detail view
-                    NavigationLink(destination: QuoteDetailView(text: quote.text ?? "",
-                                        title: quote.title ?? "",
-                                        author: quote.author ?? "",
-                                        mediaType: MediaType(rawValue: Int(quote.mediaType)) ?? MediaType.book,
-                                        quote: quote)) {
-                        QuoteItemView(quote: quote)
-                    }
-                }.onDelete(perform: self.removeQuote)
-            }
-        }.navigationBarTitle("")
-            .navigationBarHidden(true)
+    do {
+      try managedObjectContext.save()
+    } catch {
+      // handle the Core Data error
     }
-    
-    func removeQuote(at offsets: IndexSet) {
-        for index in offsets {
-            let quote = fetchRequest.wrappedValue[index]
-            managedObjectContext.delete(quote)
-        }
-        do {
-            try managedObjectContext.save()
-        } catch {
-            // handle the Core Data error
-        }
-    }
+  }
 }
